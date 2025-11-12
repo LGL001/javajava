e79f34383c4590c44181f7d29262e7169474a3d4
LGL001 <rjsqkddl1224@gmail.com>
2025-11-12 23:19:31 +0900

    9주차

---
+++ src/ch05/sec06/test.java
@@ -0,0 +1,21 @@
+package ch05.sec06;
+
+import java.util.Scanner;
+
+
+public class test {
+	
+	public static void main (String[] args) {
+	byte a = 126;
+	
+		System.out.printf("응%d",a);
+		
+		
+	}
+	
+	
+
+}
+
+java.untill.Scanner;
+
---
+++ src/ch06/sec07/exam01/CarExample.java
@@ -0,0 +1,8 @@
+package ch06.sec07.exam01;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car myCar = new Car("그랜저", "검정", 250);
+		// 실행 결과는 없지만 생성자 호출 실습입니다.
+	}
+}
