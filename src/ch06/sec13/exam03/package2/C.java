0b58f03ab38f864aac206bdfb2266154b6c98161
LGL001 <rjsqkddl1224@gmail.com>
2025-11-26 14:17:04 +0900

    12week

---
+++ src/ch06/sec13/exam03/package2/C.java
@@ -0,0 +1,19 @@
+package ch06.sec13.exam03.package2;
+
+import ch06.sec13.exam03.package1.A;
+
+public class C {
+    public C() {
+        A a = new A();
+
+        // 필드값 변경
+        a.field1 = 1; // (o)
+        // a.field2 = 1; // (x) default 필드 접근 불가 (다른 패키지)
+        // a.field3 = 1; // (x) private 필드 접근 불가
+
+        // 메소드 호출
+        a.method1(); // (o)
+        // a.method2(); // (x) default 메소드 접근 불가 (다른 패키지)
+        // a.method3(); // (x) private 메소드 접근 불가
+    }
+}
